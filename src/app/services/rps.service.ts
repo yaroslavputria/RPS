d6830bccdcd3622308a5d76098af25e9f2be905b
Yaroslav Putria <yaroslav.putria@synapse.com>
2016-11-18 16:59:10 +0200

    it works somehow

--- src/app/services/rps.service.ts
+++ src/app/services/rps.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+//import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class RPSService {
-  constructor(private http: Http) {}
-  
+//  constructor(private http: Http) {}
+
 }
