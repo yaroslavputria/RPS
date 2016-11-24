eb91b8b9786926bf45ee3efcca58b07ef9aef0c6
Yaroslav Putria <yaroslav.putria@synapse.com>
2016-11-24 16:52:30 +0200

    it fucking works

--- src/app/gamespace/gamespace.component.ts
+++ src/app/gamespace/gamespace.component.ts
@@ -13,24 +13,103 @@ export class GamespaceComponent {
 
   arrOfStats: any[] = arrOfStats;
   arrOfMoves: any[] = arrOfMoves;
+  currentSituation: string = '' + this.rpsService.randomInteger(0, 2) + this.rpsService.randomInteger(0, 2) + this.rpsService.randomInteger(0, 2) + this.rpsService.randomInteger(0, 2);
 
   bottomMes: string = 'nothing yet';
   userScore: number = 0;
   pcScore: number = 0;
+
   showOptions: boolean = false;
 
-  preMove(): void {
+  stopper: boolean = false;
 
-    setTimeout(() => {
-      this.showOptions = true;
-    }, 300);
+  preMove(): void {
+    if (!this.stopper) {
+      this.stopper = true;
+
+      setTimeout(() => {
+        this.bottomMes = 'Chose your hand';
+        this.showOptions = true;
+        this.stopper = false;
+      }, 300);
+    } else {
+      return;
+    }
   }
 
-  move(event: any, userHand: Element, pcHand: Element): void {
+  move(event: any, userHandElement: Element, pcHandElement: Element): void {
+    if (!this.stopper) {
+      this.stopper = true;
 
-    setTimeout(() => {
-      this.showOptions = false;
-    }, 300);
+      let assumedUserHand: number = this.rpsService.userHandFromStats(this.arrOfStats, this.currentSituation);
+      let pcHand: number = this.rpsService.choseMove(assumedUserHand);
+      let userHand: number = this.getUserHand(event.currentTarget.id);
+      let resOfMove: number = this.getResOfMove(userHand, pcHand);
+
+      this.renderHands(userHand, pcHand, userHandElement, pcHandElement);
+
+      this.rpsService.refreshArrOfStats(this.currentSituation, userHand, this.arrOfStats);
+
+      this.refreshCurrentSituation(userHand, resOfMove);
+
+      setTimeout(() => {
+        this.processResOfMove(resOfMove);
+        this.showOptions = false;
+        this.stopper = false;
+      }, 300);
+    } else {
+      return;
+    }
+  }
+
+  getUserHand(optionId: string): number {
+    return Number(optionId.substr(3));
+  }
+
+  getResOfMove(userHand: number, pcHand: number): number {
+    if (pcHand === userHand) {
+      return 2;
+    } else if ((userHand === 0 && pcHand === 2) || (userHand === 2 && pcHand === 1) || (userHand === 1 && pcHand === 0)) {
+      return 0;
+    } else if ((userHand === 2 && pcHand === 0) || (userHand === 1 && pcHand === 2) || (userHand === 0 && pcHand === 1)) {
+      return 1;
+    }
+  }
+
+  processResOfMove(res: number): void {
+    if (res === 0) {
+      this.userScore++;
+      this.bottomMes = 'You won this round!';
+    } else if (res === 1) {
+      this.pcScore++;
+      this.bottomMes = 'You lost this round!';
+    } else if (res === 2) {
+      this.bottomMes = 'It\'s draw!';
+    }
+  }
+
+  renderHands(userHand: number, pcHand: number, userHandElement: Element, pcHandElement: Element): void {
+    userHandElement.classList.remove('fa-hand-rock-o', 'fa-hand-paper-o', 'fa-hand-scissors-o', 'fa-flip-horizontal');
+    if (userHand === 0) {
+      userHandElement.classList.add('fa-hand-rock-o');
+    } else if (userHand === 1) {
+      userHandElement.classList.add('fa-hand-paper-o');
+    } else if (userHand === 2) {
+      userHandElement.classList.add('fa-hand-scissors-o', 'fa-flip-horizontal');
+    }
+
+    pcHandElement.classList.remove('fa-diamond', 'fa-file-o', 'fa-scissors', 'fa-flip-horizontal', 'fa-desktop');
+    if (pcHand === 0) {
+      pcHandElement.classList.add('fa-diamond');
+    } else if (pcHand === 1) {
+      pcHandElement.classList.add('fa-file-o');
+    } else if (pcHand === 2) {
+      pcHandElement.classList.add('fa-scissors', 'fa-flip-horizontal');
+    }
+  }
+
+  refreshCurrentSituation(userHand: number, resOfMove: number): void {
+    this.currentSituation = this.currentSituation.substring(2) + userHand + resOfMove;
   }
 
 }
--- src/app/services/rps.service.ts
+++ src/app/services/rps.service.ts
@@ -8,7 +8,7 @@ export class RPSService {
 
 
   // звернення до статистики
-  userHandFromStats(arrOfStats: string[], currentSituation: string): number {
+  userHandFromStats(arrOfStats: any[], currentSituation: string): number {
     let result = 0;
     let tmp = arrOfStats[currentSituation][result];
     for (let i = 1; i < 3; i++) {
@@ -29,6 +29,17 @@ export class RPSService {
     } else if (userHand === 2) {
       return 0;
     }
+  };
+
+  // оновлення масиву статистики
+  refreshArrOfStats(currentSituation: string, currentUserHand: number, arrOfStats: any[]) {
+    arrOfStats[currentSituation][currentUserHand]++;
+  }
+
+  randomInteger(min: number, max: number): number {
+    let rand = min + Math.random() * (max - min);
+    rand = Math.round(rand);
+    return rand;
   };
 
   // // поточне значення руки гравця
@@ -66,52 +77,49 @@ export class RPSService {
   //     break;
   //   };
   // };
-  // //визначення результату ходу(порівняння руки пк та гравця)
-  // function compareMoves(pcMove) {
-  //   var myMove = myCurrentHand();
-  //   if (pcMove === myMove) {
-  //     return 2;
-  //   } else if ((myMove === 0 && pcMove === 2) || (myMove === 2 && pcMove === 1) || (myMove === 1 && pcMove === 0)) {
-  //     return 0;
-  //   } else if ((myMove === 2 && pcMove === 0) || (myMove === 1 && pcMove === 2) || (myMove === 0 && pcMove === 1)) {
-  //     return 1;
-  //   };
-  // };
-  // //рухунок
-  // $scope.pcScore = 0;
-  // $scope.myScore = 0;
-  // $scope.drawScore = 0;
-  // $scope.resMessage;
-  // //опрацювання результату партії
-  // function resultProcessing(res) {
-  //   switch (res) {
-  //     case 0:
-  //     {
-  //       $scope.myScore++;
-  //       $scope.resMessage = "You won!!!";
-  //     };
-  //     break;
-  //     case 1:
-  //     {
-  //       $scope.pcScore++;
-  //       $scope.resMessage = "You lost!!!";
-  //     };
-  //     break;
-  //     case 2:
-  //             { // is it needed? :)
-  //               $scope.drawScore++;
-  //               $scope.resMessage = "Its draw!";
-  //             };
-  //             break;
-  //           };
-  //           $scope.$apply();
-  //           console.log($scope.myScore, $scope.drawScore, $scope.pcScore);
-  //         }
+//   //визначення результату ходу(порівняння руки пк та гравця)
+//   function compareMoves(pcMove) {
+//     var myMove = myCurrentHand();
+//     if (pcMove === myMove) {
+//       return 2;
+//     } else if ((myMove === 0 && pcMove === 2) || (myMove === 2 && pcMove === 1) || (myMove === 1 && pcMove === 0)) {
+//       return 0;
+//     } else if ((myMove === 2 && pcMove === 0) || (myMove === 1 && pcMove === 2) || (myMove === 0 && pcMove === 1)) {
+//       return 1;
+//     };
+//   };
+//   //рухунок
+//   $scope.pcScore = 0;
+//   $scope.myScore = 0;
+//   $scope.drawScore = 0;
+//   $scope.resMessage;
+//   //опрацювання результату партії
+//   function resultProcessing(res) {
+//     switch (res) {
+//       case 0:
+//       {
+//         $scope.myScore++;
+//         $scope.resMessage = "You won!!!";
+//       };
+//       break;
+//       case 1:
+//       {
+//         $scope.pcScore++;
+//         $scope.resMessage = "You lost!!!";
+//       };
+//       break;
+//       case 2:
+//               { // is it needed? :)
+//                 $scope.drawScore++;
+//                 $scope.resMessage = "Its draw!";
+//               };
+//               break;
+//             };
+//             $scope.$apply();
+//             console.log($scope.myScore, $scope.drawScore, $scope.pcScore);
+//           }
 
-  // //оновлення масиву статистики
-  // function refreshArrOfStats(currentSituation, currentMove) {
-  //   arrOfStats[currentSituation][currentMove]++;
-  // }
+
 
 
 }
